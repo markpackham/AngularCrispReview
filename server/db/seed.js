5f1436cf2ec08e807e2f30aeb12ef7d7c0917e1f
mark <markpackham1@gmail.com>
2022-02-03 17:09:56 +0000

    fix seeds

--- server/db/seed.js
+++ server/db/seed.js
@@ -1,26 +1,37 @@
-use "crisps_review";
+use crisps_review;
 db.dropDatabase();
 
-db.owners.insertMany([{
-    "owner" : "Nisha Enterprises incorporating Golden Cross Snacks and Millar Confectionary",
-    "owner_address" : "5A Grange Rd, Houstoun Industrial Estate, Livingston, Scotland, EH54 5DE",
-    "owner_phone" : "+44 01506 443 383",
-    "owner_website" : "https://www.nishaenterprises.co.uk/"
+db.owners.insertMany([
+  {
+    owner : "Nisha Enterprises incorporating Golden Cross Snacks and Millar Confectionary",
+    owner_address : "5A Grange Rd, Houstoun Industrial Estate, Livingston, Scotland, EH54 5DE",
+    owner_phone : "+44 01506 443 383",
+    owner_website : "https://www.nishaenterprises.co.uk/"
   },
   {
-    "owner" : "Walkers Snacks Ltd",
-    "owner_address" : "450 South Oak Way, Green Park, Reading RG2 6UW",
-    "owner_phone" : "800 274 777",
-    "owner_website" : "https://www.walkers.co.uk/"
+    owner : "Walkers Snacks Ltd",
+    owner_address : "450 South Oak Way, Green Park, Reading RG2 6UW",
+    owner_phone : "800 274 777",
+    owner_website : "https://www.walkers.co.uk/"
   }
 ]);
 
-db.brands.insertMany([{
-  "brand" : "Nisha Enterprises incorporating Golden Cross Snacks and Millar Confectionary",
-  "brand_owner" : "Golden Cross",
+db.brands.insertMany([
+{
+  brand : "Nisha Enterprises incorporating Golden Cross Snacks and Millar Confectionary",
+  brand_owner : "Golden Cross",
 },
 {
-  "brand" : "Monster Munch",
-  "brand_owner" : "Walkers Snacks Ltd",
+  brand : "Monster Munch",
+  brand_owner : "Walkers Snacks Ltd",
 }
+]);
+
+db.flavours.insertMany([
+  {
+    flavour : "Spicey",
+  },
+  {
+    flavour : "Pickled Onion",
+  }
 ]);
