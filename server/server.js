0aff3049f712924cf40e5a045297248133dea5b9
mark <markpackham1@gmail.com>
2022-02-04 07:45:55 +0000

    create brands route

--- server/server.js
+++ server/server.js
@@ -4,20 +4,24 @@ const cors = require("cors");
 app.use(cors());
 const bodyParser = require("body-parser");
 const MongoClient = require("mongodb").MongoClient;
-const createOwnersRouter = require("./helpers/create_owners_router.js");
+const createBrandsRouter = require("./helpers/create_brands_router.js");
 const createFlavoursRouter = require("./helpers/create_flavours_router.js");
+const createOwnersRouter = require("./helpers/create_owners_router.js");
 
 app.use(bodyParser.json());
 
 MongoClient.connect("mongodb://localhost:27017")
   .then((client) => {
     let db = client.db("crisps_review");
-    const ownersCollection = db.collection("owners");
-    const ownersRouter = createOwnersRouter(ownersCollection);
+    const brandsCollection = db.collection("brands");
+    const brandsRouter = createBrandsRouter(brandsCollection);
     const flavoursCollection = db.collection("flavours");
     const flavoursRouter = createFlavoursRouter(flavoursCollection);
-    app.use("/", ownersRouter);
+    const ownersCollection = db.collection("owners");
+    const ownersRouter = createOwnersRouter(ownersCollection);
+    app.use("/", brandsRouter);
     app.use("/", flavoursRouter);
+    app.use("/", ownersRouter);
   })
 
   .catch(console.err);
