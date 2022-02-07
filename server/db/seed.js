cbb4e2ab17a4d830c07fc6626937e861d087ce69
mark <markpackham1@gmail.com>
2022-02-07 08:27:48 +0000

    add additional seeds

--- server/db/seed.js
+++ server/db/seed.js
@@ -5,7 +5,7 @@ db.owners.insertMany([
   {
     owner : "Nisha Enterprises incorporating Golden Cross Snacks and Millar Confectionary",
     owner_address : "5A Grange Rd, Houstoun Industrial Estate, Livingston, Scotland, EH54 5DE",
-    owner_phone : "+44 01506 443 383",
+    owner_phone : "01506 443 383",
     owner_website : "https://www.nishaenterprises.co.uk/"
   },
   {
@@ -13,17 +13,33 @@ db.owners.insertMany([
     owner_address : "450 South Oak Way, Green Park, Reading RG2 6UW",
     owner_phone : "800 274 777",
     owner_website : "https://www.walkers.co.uk/"
+  },
+  {
+    owner: "Kellogg Co.",
+    owner_address: "Orange Tower Media City Uk, Salford, Greater Manchester, United Kingdom, M50 2HF",
+    owner_phone: "0800 626066",
+    owner_website: "https://www.kelloggs.co.uk/en_GB/home.html"
+  },
+  {
+    owner: "KP Snacks Limited",
+    owner_address: "Fifth Floor, The Urban Building, 3-9 Albert Street, Slough, SL1 2BE",
+    owner_phone: "0800 917 4494",
+    owner_website: "https://www.kpsnacks.com/"
   }
 ]);
 
 db.brands.insertMany([
 {
-  brand : "Nisha Enterprises incorporating Golden Cross Snacks and Millar Confectionary",
-  brand_owner : "Golden Cross",
+  brand : "Golden Cross",
+  brand_owner : "Nisha Enterprises incorporating Golden Cross Snacks and Millar Confectionary",
 },
 {
   brand : "Monster Munch",
   brand_owner : "Walkers Snacks Ltd",
+},
+{
+  brand: "Pringles",
+  brand_owner: "Kellogg Co."
 }
 ]);
 
