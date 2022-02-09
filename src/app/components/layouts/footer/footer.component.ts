5f4538d784e474b07ac121a798673099ecdcf9bc
mark <markpackham1@gmail.com>
2022-02-09 11:00:44 +0000

    display header and footer

--- src/app/components/layouts/footer/footer.component.ts
+++ src/app/components/layouts/footer/footer.component.ts
@@ -9,6 +9,8 @@ export class FooterComponent implements OnInit {
 
   constructor() { }
 
+  currentDate = new Date();
+
   ngOnInit(): void {
   }
 
--- src/app/components/layouts/header/header.component.ts
+++ src/app/components/layouts/header/header.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
+  title: string = 'Crisp Review';
+
   constructor() { }
 
   ngOnInit(): void {
