681c1c6997266b906058ef3c2896bd35ca77b305
mark <markpackham1@gmail.com>
2022-02-09 10:47:28 +0000

    move header and footer to new location

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,22 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './layouts/header/header.component';
-import { FooterComponent } from './layouts/footer/footer.component';
 import { FlavourComponent } from './components/flavour/flavour.component';
 import { BrandComponent } from './components/brand/brand.component';
 import { CrispComponent } from './components/crisp/crisp.component';
 import { OwnerComponent } from './components/owner/owner.component';
+import { HeaderComponent } from './components/layouts/header/header.component';
+import { FooterComponent } from './components/layouts/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent,
-    FooterComponent,
     FlavourComponent,
     BrandComponent,
     CrispComponent,
-    OwnerComponent
+    OwnerComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
