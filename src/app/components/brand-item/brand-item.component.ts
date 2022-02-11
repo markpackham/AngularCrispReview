fce98815ab5ba7f70dfa91061d74e5cd0295067e
mark <markpackham1@gmail.com>
2022-02-11 13:09:14 +0000

    output brand data

--- src/app/components/brand-item/brand-item.component.ts
+++ src/app/components/brand-item/brand-item.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Brand } from '../../model/Brand';
 
 @Component({
   selector: 'app-brand-item',
@@ -6,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./brand-item.component.css']
 })
 export class BrandItemComponent implements OnInit {
+  @Input() brand: Brand;
 
   constructor() { }
 
