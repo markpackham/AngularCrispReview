8f460c202d9b0f12e594a846bc4172d40967cc82
mark <markpackham1@gmail.com>
2022-02-14 07:51:26 +0000

    add delete functionality

--- src/app/components/brand-item/brand-item.component.ts
+++ src/app/components/brand-item/brand-item.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { Brand } from '../../model/Brand';
 
 @Component({
@@ -8,10 +8,15 @@ import { Brand } from '../../model/Brand';
 })
 export class BrandItemComponent implements OnInit {
   @Input() brand: Brand;
+  @Output() onDeleteBrand: EventEmitter<Brand> = new EventEmitter();
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onDelete(brand: Brand) {
+    this.onDeleteBrand.emit(brand);
   }
 
 }
