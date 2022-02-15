a6157fa9d5d502b81e6946240ab066a24070039d
mark <markpackham1@gmail.com>
2022-02-15 12:14:25 +0000

    add Brands to database
    https://youtu.be/ACsVNDIuKV8?t=2543

--- src/app/components/add-brand/add-brand.component.ts
+++ src/app/components/add-brand/add-brand.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { Subscription } from 'rxjs';
-import { Brand } from '../../model/Brand';
 import {FormControl, FormGroup, Validators} from '@angular/forms';
+import { Brand } from '../../model/Brand';
+import { BrandService } from '../../services/brand.service';
 
 @Component({
   selector: 'app-add-brand',
@@ -13,7 +14,7 @@ export class AddBrandComponent implements OnInit {
   brand_name!: string;
   brand_owner!: string;
 
-  constructor() { }
+  constructor(private service: BrandService) { }
 
   ngOnInit(): void {
   }
@@ -24,16 +25,11 @@ export class AddBrandComponent implements OnInit {
   });
 
   onSubmit() {
-    console.log(this.brandForm.value);
-
-    const newBrand: Brand = {
-      brand_name : this.brand_name,
-      brand_owner: this.brand_owner
-    };
-
-    this.onAddBrand.emit(newBrand);
-    this.brand_name = '';
-    this.brand_owner = '';
+    if(this.brandForm.valid){
+      this.service.addBrand(this.brandForm.value).subscribe((res)=>{
+          this.brandForm.reset();
+      });
+    }
   }
 
 }
