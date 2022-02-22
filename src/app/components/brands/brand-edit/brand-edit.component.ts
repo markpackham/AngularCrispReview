b75c90650d4df1ddf8a79d2cfd72a41a72d0d728
mark <markpackham1@gmail.com>
2022-02-22 09:16:32 +0000

    modify brand edit form

--- src/app/components/brands/brand-edit/brand-edit.component.ts
+++ src/app/components/brands/brand-edit/brand-edit.component.ts
@@ -1,7 +1,7 @@
-import { Component, OnInit, Input} from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { Brand } from '../../../model/Brand';
 import { BrandService } from '../../../services/brand.service';
-import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-brand-edit',
@@ -9,11 +9,33 @@ import { ActivatedRoute } from '@angular/router';
   styleUrls: ['./brand-edit.component.css']
 })
 export class BrandEditComponent implements OnInit {
-  @Input() brand: Brand;
+  @Output() onBrandAdd: EventEmitter<Brand> = new EventEmitter();
+  brand_name!: string;
+  brand_owner!: string;
+  errorMsg:any;
+  successMsg:any;
 
-  constructor(private service: BrandService, private router: ActivatedRoute) { }
+  constructor(private service: BrandService) { }
 
   ngOnInit(): void {
+  }
+
+  brandForm = new FormGroup({
+    'brand_name':new FormControl('',[Validators.required, Validators.minLength(3)]),
+    'brand_owner':new FormControl('',[Validators.required, Validators.minLength(3)]),
+  });
+
+  onSubmit() {
+    if(this.brandForm.valid){
+      this.service.addBrand(this.brandForm.value).subscribe((res)=>{
+          location.reload();
+          this.brandForm.reset();
+          this.successMsg = "Creation successful!";
+      });
+    }
+    else{
+      this.errorMsg = 'All fields required!';
+    }
   }
 
 }
