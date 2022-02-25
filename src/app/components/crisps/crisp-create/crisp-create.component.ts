d191c23d3b5a2cff2ce21f1bdd82fa1638688be2
mark <markpackham1@gmail.com>
2022-02-25 09:15:39 +0000

    get brands and flavours

--- src/app/components/crisps/crisp-create/crisp-create.component.ts
+++ src/app/components/crisps/crisp-create/crisp-create.component.ts
@@ -1,8 +1,12 @@
 import { ActivatedRoute } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
 import {FormControl, FormGroup, Validators} from '@angular/forms';
+import { Brand } from '../../../model/Brand';
 import { Crisp } from '../../../model/Crisp';
+import { Flavour } from '../../../model/Flavour';
+import { BrandService } from '../../../services/brand.service';
 import { CrispService } from '../../../services/crisp.service';
+import { FlavourService } from '../../../services/flavour.service';
 
 @Component({
   selector: 'app-crisp-create',
@@ -11,7 +15,9 @@ import { CrispService } from '../../../services/crisp.service';
 })
 export class CrispCreateComponent implements OnInit {
 
+  brands: Brand[] = [];
   crisps: Crisp[] = [];
+  flavours: Flavour[] = [];
  
   crispForm = new FormGroup({
     'crisp_name':new FormControl('',[Validators.required, Validators.minLength(3)]),
@@ -27,7 +33,7 @@ export class CrispCreateComponent implements OnInit {
   successMsg!: string;
   getParamId: any;
 
-  constructor(private service: CrispService, private router: ActivatedRoute) { }
+  constructor(private service: CrispService, private serviceBrand: BrandService, private serviceFlavour: FlavourService, private router: ActivatedRoute) { }
 
   ngOnInit(): void {
     this.getParamId = this.router.snapshot.paramMap.get('id');
@@ -73,6 +79,14 @@ export class CrispCreateComponent implements OnInit {
     else{
       this.errorMsg = 'Only the image, review and review score can be empty!';
     }
+  }
+
+  getAllBrands(){
+    this.serviceBrand.getBrands().subscribe((brands) => (this.brands = brands));
+  }
+
+  getAllFlavours(){
+    this.serviceFlavour.getFlavours().subscribe((flavours) => (this.flavours = flavours));
   }
 
 }
