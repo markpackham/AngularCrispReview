df85038fafddb3b7777990dd64c7c68721d793a0
mark <markpackham1@gmail.com>
2022-02-28 20:51:00 +0000

    check for unique crisp name

--- src/app/services/custom-validation.service.ts
+++ src/app/services/custom-validation.service.ts
@@ -11,7 +11,7 @@ export class CustomValidationService {
 
   constructor(private http: HttpClient) { }
 
-
+  // Respond as to whether or not the brand name is taken 
   validateBrandNameNotTaken(control: AbstractControl) {
     return this.checkBrandNameNotTaken(control.value).pipe(
       map(res => {
@@ -20,12 +20,31 @@ export class CustomValidationService {
     );
   }
 
+  // See if brand name already taken in database
   checkBrandNameNotTaken(brand_name: string): Observable<boolean> {
     return this.http.get("http://localhost:3000/brands").pipe(
       map((brandList: any) => 
       brandList.filter((brand: { brand_name: string; }) => brand.brand_name === brand_name)
       ),
       map(brands => !brands.length)
+    );
+  }
+
+
+  validateCrispNameNotTaken(control: AbstractControl) {
+    return this.checkCrispNameNotTaken(control.value).pipe(
+      map(res => {
+        return res ? null : { crisp_nameTaken: true };
+      })
+    );
+  }
+
+  checkCrispNameNotTaken(crisp_name: string): Observable<boolean> {
+    return this.http.get("http://localhost:3000/crisps").pipe(
+      map((crispList: any) => 
+      crispList.filter((crisp: { crisp_name: string; }) => crisp.crisp_name === crisp_name)
+      ),
+      map(crisps => !crisps.length)
     );
   }
 
