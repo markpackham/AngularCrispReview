2dacb946d05be0f8901dd4b31bf0c289138319d8
mark <markpackham1@gmail.com>
2022-03-03 23:55:00 +0000

    add toggle to show best and worst crisps

--- src/app/components/crisps/crisp/crisp.component.ts
+++ src/app/components/crisps/crisp/crisp.component.ts
@@ -10,9 +10,12 @@ import { CrispService } from '../../../services/crisp.service';
 export class CrispComponent implements OnInit {
 
   crisps: Crisp[] = [];
+  crispsMessage: string = "Show Worst Crisps";
+  crispsWorst: boolean = true;
   orderReviewScore: string = 'review_score';
 
-  constructor(private service: CrispService) { 
+  constructor(private service: CrispService) {
+    this.crispsWorst === true; 
   }
 
   ngOnInit(): void {
@@ -21,6 +24,19 @@ export class CrispComponent implements OnInit {
 
   getAllCrisps(){
     this.service.getCrisps().subscribe((crisps) => (this.crisps = crisps));
+  }
+
+  showWorstCrisps(){
+
+    if(this.crispsWorst === true)
+    {
+      this.crispsMessage = "Show Best Crisps";
+      this.crispsWorst = false;
+    }
+    else{
+      this.crispsMessage = "Show Worst Crisps";
+      this.crispsWorst = true;
+    }
   }
 
 }
