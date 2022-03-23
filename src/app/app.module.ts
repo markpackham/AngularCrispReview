35a9f6bc1ac93003210828c44120a1f3be9c5e2b
mark <markpackham1@gmail.com>
2022-03-23 13:11:06 +0000

    add MatAutocompleteModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
 import { MatCardModule } from '@angular/material/card';
 import { MatDividerModule } from '@angular/material/divider';
 import { MatExpansionModule } from '@angular/material/expansion';
@@ -69,6 +70,7 @@ import { DataTableComponent } from './data-table/data-table.component';
     BrowserAnimationsModule,
     FormsModule,
     HttpClientModule,
+    MatAutocompleteModule,
     MatCardModule,
     MatDividerModule,
     MatExpansionModule,
