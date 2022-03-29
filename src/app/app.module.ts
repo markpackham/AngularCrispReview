bf214d494470187e66a06cf81554a81d2f1e2135
mark <markpackham1@gmail.com>
2022-03-29 10:30:59 +0100

    sort order of module imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,5 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { MatAutocompleteModule } from '@angular/material/autocomplete';
@@ -12,61 +12,61 @@ import { MatSortModule } from '@angular/material/sort';
 import { MatTableModule } from '@angular/material/table';
 import { MatTabsModule } from '@angular/material/tabs';
 import { MatTooltipModule } from '@angular/material/tooltip';
-import { OrderModule } from 'ngx-order-pipe';
 import { NgModule } from '@angular/core';
+import { OrderModule } from 'ngx-order-pipe';
 
 import { AboutComponent } from './content/about/about.component';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { AsideComponent } from './layouts/aside/aside.component';
 import { BrandComponent } from './components/brands/brand/brand.component';
+import { BrandCreateComponent } from './components/brands/brand-create/brand-create.component';
+import { BrandReadComponent } from './components/brands/brand-read/brand-read.component';
 import { ContactComponent } from './content/contact/contact.component';
 import { CrispComponent } from './components/crisps/crisp/crisp.component';
+import { CrispCreateComponent } from './components/crisps/crisp-create/crisp-create.component';
+import { CrispReadComponent } from './components/crisps/crisp-read/crisp-read.component';
 import { CrudService } from './services/crud.service';
 import { FaqComponent } from './content/faq/faq.component';
 import { FlavourComponent } from './components/flavours/flavour/flavour.component';
+import { FlavourCreateComponent } from './components/flavours/flavour-create/flavour-create.component';
+import { FlavourReadComponent } from './components/flavours/flavour-read/flavour-read.component';
 import { FooterComponent } from './layouts/footer/footer.component';
 import { HeaderComponent } from './layouts/header/header.component';
 import { HomeComponent } from './content/home/home.component';
 import { NotFoundComponent } from './helpers/not-found/not-found.component';
 import { OwnerComponent } from './components/owners/owner/owner.component';
-import { BrandCreateComponent } from './components/brands/brand-create/brand-create.component';
-import { BrandReadComponent } from './components/brands/brand-read/brand-read.component';
-import { CrispReadComponent } from './components/crisps/crisp-read/crisp-read.component';
-import { CrispCreateComponent } from './components/crisps/crisp-create/crisp-create.component';
-import { FlavourCreateComponent } from './components/flavours/flavour-create/flavour-create.component';
-import { FlavourReadComponent } from './components/flavours/flavour-read/flavour-read.component';
-import { OwnerReadComponent } from './components/owners/owner-read/owner-read.component';
 import { OwnerCreateComponent } from './components/owners/owner-create/owner-create.component';
-import { AsideComponent } from './layouts/aside/aside.component';
+import { OwnerReadComponent } from './components/owners/owner-read/owner-read.component';
 
 @NgModule({
   declarations: [
     AboutComponent,
     AppComponent,
+    AsideComponent,
     BrandComponent,
+    BrandCreateComponent,
+    BrandReadComponent,
     ContactComponent,
     CrispComponent,
+    CrispCreateComponent,
+    CrispReadComponent,
+    FaqComponent,
     FlavourComponent,
+    FlavourCreateComponent,
+    FlavourReadComponent,
     FooterComponent,
     HeaderComponent,
     HomeComponent,
     NotFoundComponent,
     OwnerComponent,
-    BrandCreateComponent,
-    BrandReadComponent,
-    CrispReadComponent,
-    CrispCreateComponent,
-    FlavourCreateComponent,
-    FlavourReadComponent,
-    OwnerReadComponent,
     OwnerCreateComponent,
-    AsideComponent,
-    FaqComponent,
+    OwnerReadComponent,
   ],
   imports: [
     AppRoutingModule,
-    BrowserModule,
     BrowserAnimationsModule,
+    BrowserModule,
     FormsModule,
     HttpClientModule,
     MatAutocompleteModule,
