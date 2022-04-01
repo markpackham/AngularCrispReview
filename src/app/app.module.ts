254f1ea9be6a65e3360543c8924d7767441a8ae3
mark <markpackham1@gmail.com>
2022-04-01 12:45:23 +0100

    output read only potato data

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import { MatDialogModule } from '@angular/material/dialog';
 import { MatDividerModule } from '@angular/material/divider';
 import { MatExpansionModule } from '@angular/material/expansion';
 import { MatTabsModule } from '@angular/material/tabs';
+import { MatTableModule } from '@angular/material/table';
 import { MatTooltipModule } from '@angular/material/tooltip';
 import { NgModule } from '@angular/core';
 import { OrderModule } from 'ngx-order-pipe';
@@ -74,6 +75,7 @@ import { PotatoesComponent } from './components/read-only/potatoes/potatoes.comp
     MatDividerModule,
     MatExpansionModule,
     MatTabsModule,
+    MatTableModule,
     MatTooltipModule,
     OrderModule,
     ReactiveFormsModule,
--- src/app/components/read-only/potatoes/potatoes.component.ts
+++ src/app/components/read-only/potatoes/potatoes.component.ts
@@ -1,5 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 
+export interface PotatoStructure {
+  potato_name: string;
+  potato_country: string;
+}
+
+const POTATO_DATA: PotatoStructure[] = [
+  {potato_name: 'King Edward', potato_country: 'UK'},
+  {potato_name: 'Yukon Gold', potato_country: 'Canada'},
+];
+
 @Component({
   selector: 'app-potatoes',
   templateUrl: './potatoes.component.html',
@@ -11,5 +21,8 @@ export class PotatoesComponent implements OnInit {
 
   ngOnInit(): void {
   }
+
+  displayedColumns: string[] = ['potato_name', 'potato_country'];
+  dataSource = POTATO_DATA;
 
 }
