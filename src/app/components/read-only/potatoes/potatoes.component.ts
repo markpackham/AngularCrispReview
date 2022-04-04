2f40db464c43786bd85e621e13af3eb1a848c278
mark <markpackham1@gmail.com>
2022-04-04 13:10:33 +0100

    remove obsolete class Sort

--- src/app/components/read-only/potatoes/potatoes.component.ts
+++ src/app/components/read-only/potatoes/potatoes.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { MatPaginator, PageEvent } from '@angular/material/paginator';
-import { MatSort, Sort } from '@angular/material/sort';
+import { MatSort } from '@angular/material/sort';
 import { MatTableDataSource } from '@angular/material/table';
 import { Potato } from '../../../model/Potato';
 import { PotatoesService } from '../../../services/potatoes.service';
