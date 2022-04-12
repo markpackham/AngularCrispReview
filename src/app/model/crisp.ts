2dcb9b70926ff69b7b76d0e4aac66e00130b3ef1
mark <markpackham1@gmail.com>
2022-04-12 12:17:41 +0100

    add image upload to model

--- src/app/model/crisp.ts
+++ src/app/model/crisp.ts
@@ -2,6 +2,7 @@ export interface Crisp {
     _id?: number;
     crisp_name : string;
     crisp_image?: string;
+    crisp_image_upload?: string;
     flavour_name : string;
     brand_name: string;
     weight: number;
